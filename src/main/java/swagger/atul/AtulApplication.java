92fcbd616e5f7e3f5272292933487512d43d3f98
Atul Kumar Gupta <54747380+AtulKumarIndian@users.noreply.github.com>
2024-09-13 22:35:36 +0530

    basic springboot project for the Swagger UI

---
+++ src/main/java/swagger/atul/AtulApplication.java
@@ -0,0 +1,13 @@
+package swagger.atul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/swagger/atul/AtulApplicationTests.java
@@ -0,0 +1,13 @@
+package swagger.atul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
